1ce0b90fd2d676fea059856ab5c4abc0e60d6d9a
Mykolas Raudzius <raudziusmykolas@gmail.com>
2023-04-30 17:39:22 +0300

    feat: fetch activities from api

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -1,5 +1,24 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 
-const App: React.FC = () => <div>App</div>;
+const App: React.FC = () => {
+  const [activities, setActivities] = useState([]);
+
+  useEffect(() => {
+    axios.get('http://localhost:5000/api/activities').then((response) => {
+      setActivities(response.data);
+    });
+  }, []);
+
+  return (
+    <div>
+      <ul>
+        {activities.map(({ id, title }: any) => (
+          <li key={id}>{title}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
 
 export default App;
